b3378572fcb16331673f4295368297793b06a9ce
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-20 14:14:22 +0200

    Added helper function
    - getHCFrontEndLanguages
    - getHCBackEndLanguages
    - getHCContentLanguages

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,1 +1,42 @@
 <?php
+
+use interactivesolutions\honeycomblanguages\app\models\HCLanguages;
+
+if (!function_exists ('getHCFrontEndLanguages')) {
+
+    /**
+     * Getting available frontEnd languages
+     *
+     * @return mixed
+     */
+    function getHCFrontEndLanguages()
+    {
+        return HCLanguages::where('front_end', 1)->select('id', 'iso_639_1', 'language')->get();
+    }
+}
+
+if (!function_exists ('getHCBackEndLanguages')) {
+
+    /**
+     * Getting available backend languages
+     *
+     * @return mixed
+     */
+    function getHCBackEndLanguages()
+    {
+        return HCLanguages::where('back_end', 1)->select('id', 'iso_639_1', 'language')->get();
+    }
+}
+
+if (!function_exists ('getHCContentLanguages')) {
+
+    /**
+     * Getting available content languages
+     *
+     * @return mixed
+     */
+    function getHCContentLanguages()
+    {
+        return HCLanguages::where('content', 1)->select('id', 'iso_639_1', 'language')->get();
+    }
+}
