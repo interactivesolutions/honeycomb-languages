ec13e4ec52620b6c7047a4a2fa554b96b9e3eeb5
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-04-17 11:46:29 +0300

    Updated routes middleware

--- src/app/routes/api/routes.languages.php
+++ src/app/routes/api/routes.languages.php
@@ -1,15 +1,15 @@
 <?php
 
-Route::group(['prefix' => 'api', 'middleware' => ['web', 'auth-apps']], function ()
+Route::group(['prefix' => 'api', 'middleware' => ['auth-apps']], function ()
 {
-    Route::get('languages', ['as' => 'api.languages', 'middleware' => ['acl:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@adminView']);
+    Route::get('languages', ['as' => 'api.languages', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@adminView']);
 
     Route::group(['prefix' => 'v1/languages'], function ()
     {
-        Route::get('/', ['as' => 'api.v1.api.languages', 'middleware' => ['acl:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@listPage']);
-        Route::get('list/{timestamp}', ['as' => 'api.v1.api.languages.list.update', 'middleware' => ['acl:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@listUpdate']);
-        Route::get('list', ['as' => 'api.v1.api.languages.list', 'middleware' => ['acl:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@list']);
-        Route::get('search', ['as' => 'api.v1.api.languages.search', 'middleware' => ['acl:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@listSearch']);
-        Route::put('{id}/strict', ['as' => 'api.v1.api.languages.update.strict', 'middleware' => ['acl:interactivesolutions_honeycomb_languages_languages_update'], 'uses' => 'HCLanguagesController@updateStrict']);
+        Route::get('/', ['as' => 'api.v1.api.languages', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@listPage']);
+        Route::get('list/{timestamp}', ['as' => 'api.v1.api.languages.list.update', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@listUpdate']);
+        Route::get('list', ['as' => 'api.v1.api.languages.list', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@list']);
+        Route::get('search', ['as' => 'api.v1.api.languages.search', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_languages_languages_list'], 'uses' => 'HCLanguagesController@listSearch']);
+        Route::put('{id}/strict', ['as' => 'api.v1.api.languages.update.strict', 'middleware' => ['acl-apps:interactivesolutions_honeycomb_languages_languages_update'], 'uses' => 'HCLanguagesController@updateStrict']);
     });
 });
