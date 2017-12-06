941763e8ea2ab9f1c73888307833e5cb4b5996a6
vytautas <vytautas.rimeikis@gmail.com>
2017-12-06 10:35:13 +0200

    fix searchQuery

--- src/app/http/controllers/HCLanguagesController.php
+++ src/app/http/controllers/HCLanguagesController.php
@@ -135,7 +135,7 @@ class HCLanguagesController extends HCBaseController
      * @param string $phrase
      * @return Builder
      */
-    protected function searchQuery(Builder $query, string $phrase)
+    protected function searchQuery(Builder $query, string $phrase): Builder
     {
         return $query->where(function(Builder $query) use ($phrase) {
             $query->where('language_family', 'LIKE', '%' . $phrase . '%')
